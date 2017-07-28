b02d51da904fd1ee0f6a293c404f1e9660ef7b31
yago1994 <yagoaral@hotmail.com>
2017-07-27 23:15:17 -0700

    Add files via upload

---
+++ IntelHacks_RPi.py
@@ -0,0 +1,16 @@
+from firebase import firebase
+import serial
+
+arduinoSerialData = serial.Serial('/dev/ttyACM1',9600)
+firebase = firebase.FirebaseApplication('https://yourdatabase.firebaseio.com/',None)
+
+while(1):
+    line = arduinoSerialData.readline()
+    if int(line) == 1:
+        print line
+        new_user = 'Address'
+        new_serial = 'Serial - 4a3re89'
+        result = firebase.post('/Address',new_address)
+    else:
+        print line
+        print 'no hit'
